157651ddbb9600f91a326398bb8ffb9d6c99fe18
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-29 17:17:23 +0100

    Hvis Array Words er tom, returneres emppty
    Yderligere regnefunktioner tilføjet i Caucolator

--- js/main.js
+++ js/main.js
@@ -3,11 +3,12 @@ const app = Vue.createApp({
         return {
             intro: 'Welcome to my Vue template',
         Word: '',
-        Words: ['hey','you'],
+        Words: ['hey','honney'],
         Message:'',
-        Number1:'',
-        Number2:'',
-        Result:''
+        Number1:'4',
+        Number2:'6',
+        Result:'',
+        Operation:''
         }
     },
     methods: {
@@ -16,16 +17,57 @@ const app = Vue.createApp({
         },
         Save(){
             this.Words.push(this.Word)
+            this.Word=''
+            this.Show()
         },
         Show(){
+            if(this.Words.length===0){
+                this.Message="<i>Empty</i>"
+            }
+            else{
+                this.Message = this.Words.join(', ');
+            }
+            
 
-            this.Message = this.Words.join(', ');
         },
         Clear(){
             this.Words=[]
+            this.Show()
         },
         Add(){
             this.Result= this.Number1+ this.Number2
+        },
+        SwitchOperation()
+        {
+            const num1 = Number(this.Number1)
+            const num2 = Number(this.Number2)
+
+            switch (this.Operation) {
+            case '+':
+                {
+                    this.Result= num1+num2
+                    break;
+                }
+            case "-":
+                {
+                    this.Result= num1-num2
+                    
+                    break;
+                }
+            case "*":
+                {
+                    this.Result= num1* num2
+                    break;
+                }
+            case "/":
+                {
+                    this.Result= num2==0? "Du kan ikke dividere med 0":num1/num2
+                    break;
+                }
+            default:{
+                break;
+            }
+        }
         }
     },
     computed: {
