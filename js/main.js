8a65af6d62fa28c8872a0373d5ab278b3fc5f8e0
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-10-29 12:26:51 +0100

    Funktionel og færdig Collect Word
    Funktionel Calculator med Add funktion

--- js/main.js
+++ js/main.js
@@ -2,12 +2,31 @@ const app = Vue.createApp({
     data() {
         return {
             intro: 'Welcome to my Vue template',
+        Word: '',
+        Words: ['hey','you'],
+        Message:'',
+        Number1:'',
+        Number2:'',
+        Result:''
         }
     },
     methods: {
         myMethod(){
 
         },
+        Save(){
+            this.Words.push(this.Word)
+        },
+        Show(){
+
+            this.Message = this.Words.join(', ');
+        },
+        Clear(){
+            this.Words=[]
+        },
+        Add(){
+            this.Result= this.Number1+ this.Number2
+        }
     },
     computed: {
         myComputed() {
